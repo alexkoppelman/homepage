a2b1949b4fbc4f85d934a2e3ab3ed1f5ec226e25
alexkoppelman <alex.k.bcn@gmail.com>
2022-10-07 15:36:45 +0200

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/components/links.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Footer from './components/footer.js'
 import Header from './components/header.js'
 import Aboutme from './components/aboutme.js'
+import Links from './components/links.js';
 
 function App() {
   return (
     
     <div className="App">
       <Header />
+      {/* the about me section should be hidden and shown only when hoovered? */}
       <Aboutme />
+      {/* the Links area*/}
+      <Links />
+      {/* */}
       <Footer />
     </div>
   );
---
+++ src/components/links.js
@@ -0,0 +1,19 @@
+export default function Links() {
+    return (
+       
+       <div className="links">
+       <span className="linkstext">
+        <ul>
+            <li>Minecraft Server</li>
+            <li>Viletans</li>
+            <li>GitHub</li>
+            <li>LinkedIn</li>
+            <li>Twitter</li>
+        </ul>
+
+
+       </span>
+       </div>
+       
+   )
+   }
