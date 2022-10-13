51f58e461eb12928fc1fa90cee62938e76f5474e
alexkoppelman <alex.k.bcn@gmail.com>
2022-10-13 16:32:15 +0200

    modified:   package-lock.json 	modified:   package.json 	new file:   public/images/0.png 	new file:   public/images/2.png 	new file:   public/images/3.png 	new file:   public/images/4.png 	new file:   public/images/5.png 	modified:   src/App.js 	modified:   src/components/skills.js 	new file:   src/images/0.png 	new file:   src/images/2.png 	new file:   src/images/3.png 	new file:   src/images/4.png 	new file:   src/images/5.png

--- src/App.js
+++ src/App.js
@@ -6,47 +6,60 @@ import Aboutme from './components/aboutme.js';
 import Links from './components/links.js';
 import Skills from './components/skills.js';
 import Projects from './components/projects';
+import React from "react";
 
-import { useState, useEffect } from 'React'
 
-function MyComponent() {
-  const [items, setItems] = useState([])
-
-  useEffect(() => {
-    fetch('https://192.168.1.100:1880/skills')
-      .then((res) => res.json())
-      .then((resJson) => {
-        const data = JSON.parse(resJson)
-    })
-  }, [])
-}
 
 function App() {
+  
+  
+  
+  const [skillsArray, setSkillsArray] = React.useState();
+  // Function to collect data
+  const getApiData = async () => {
+  const response = await fetch(
+    "https://alexkoppelman.es:1880/skills"
+  ).then((response) => response.json());
+  
+  setSkillsArray(response);
+};
 
-  const [skillsArray, setSkillsArray] = React.useState(data)
+React.useEffect(() => {
+  getApiData()
+  }, []);
 
-  const skills = skillsArray.map(item => {
-    return (
-        <Skills 
-            id={item.id}
-            name={item.skillName}
-            rating={item.skillRating}
-        />
+  const skilllist = skillsArray && 
+    skillsArray.map((item => {
+      console.log(item);
+      return(      
+      
+      <Skills 
+          key={item.idskills}
+          name={item.SkillName}
+          rating={item.SkillRating}
+      />
+      )
+  })
     )
-  })        
+ 
+  
+  return (
 
-
-   return (
-    
     <div className="App">
 
+    
+        
+  
       {/* The Headooooor */}
       <Header />
       {/* The about me section should be hidden and shown only when hoovered? */}
       <div className="mainpage">
       <Aboutme />
       {/* The Skills area*/}
-      <Skills />
+      <section className='skillsSection'>
+      {console.log(skilllist)}
+      {skilllist}
+      </section>
       { /* The Projects area*/}
       <Projects />
       { /* The Links area*/}
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,15 +1,12 @@
+import React from "react";
 export default function Skills(props) {
 
+let image = props.rating & ".png" ;
+
   return (
-       <div className="skills">
-          <p>
-            <b>Skills</b>
-          </p>
-          <p>
-           
-        
-   
-          </p>
-       </div>
+    <div className="skills" key={props.idskills} >
+    <h3>{props.name}</h3>
+    <h3><img src={`/images/${props.rating}.png`} /></h3>
+    </div>
    )
    } 
