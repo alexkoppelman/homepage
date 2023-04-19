d1a7b559ad0d745f94c484beabbfd61ac194b9f0
alex koppelman <alex.k.bcn@gmail.com>
2023-04-19 19:24:36 +0200

    new file:   public/images/MetaMask_Fox.svg.png 	new file:   public/images/password.jpg 	modified:   src/App.css 	modified:   src/components/header.js 	modified:   src/css/gridcss.css 	new file:   src/images/MetaMask_Fox.svg.png 	modified:   src/proj.json

--- src/components/header.js
+++ src/components/header.js
@@ -6,10 +6,10 @@ export default function Header() {
             <div className="header--foto"><img src="images/me_Small.jpg" alt="Me" className="headerpicture"/></div>
             <div className="headername">Alex Koppelman | <b>Developer</b></div>
             <div className="header--socials">
-              <a href="https://twitter.com/alexkbcn"><i className="fa-brands fa-square-twitter"></i></a>
-              <a href="https://github.com/alexkoppelman"><i className="fa-brands fa-square-github"></i></a>
-              <a href="https://linkedin.com/alexkoppelman"><i className="fa-brands fa-linkedin"></i></a>
-              <a href="mailto:alexkoppelman@protonmail.com"><i className="fa-solid fa-square-envelope"></i></a>
+              <span className="header--social-icon"><a href="https://twitter.com/alexkbcn"><i className="fa-brands fa-square-twitter"></i></a></span>
+              <span className="header--social-icon"><a href="https://github.com/alexkoppelman"><i className="fa-brands fa-square-github"></i></a></span>
+              <span className="header--social-icon"><a href="https://linkedin.com/alexkoppelman"><i className="fa-brands fa-linkedin"></i></a></span>
+              <span className="header--social-icon"><a href="mailto:alexkoppelman@protonmail.com"><i className="fa-solid fa-square-envelope"></i></a></span>
               </div>
           </div>
       </header>
